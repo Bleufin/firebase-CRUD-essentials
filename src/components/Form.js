0bede834755411a9502947cacc4262b2deb11406
Bluefin <tuna.malik@bluefin.com.tr>
2020-09-27 02:32:59 +0300

    finished Create part of CRUD

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
+import firebase from "../util/firebase";
 
 function Form() {
   const [title, setTitle] = useState("");
   const handleOnChange = e => {
     setTitle(e.target.value);
   };
+  const createTodo = () => {
+    const todoRef = firebase.database().ref("Todo");
+    const todo = {
+      title,
+      complete: false
+    };
+
+    todoRef.push(todo);
+  };
 
   return (
     <div>
       <input type="text" onChange={handleOnChange} value={title} />
-      <button>Add Todo</button>
+      <button onClick={createTodo}>Add Todo</button>
     </div>
   );
 }
