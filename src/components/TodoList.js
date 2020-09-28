47b857d8fb9d5768c1f5116d70e64d51758a5c0d
Bluefin <tuna.malik@bluefin.com.tr>
2020-09-28 18:33:02 +0300

    sıralama OK

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,22 +7,23 @@ function TodoList() {
   const [todoList, setToDoList] = useState();
 
   useEffect(() => {
-    const todoRef = firebase.database().ref("Todo");
+    const todoRef = firebase.database().ref("Todo").orderByChild("title");
 
     // database'den veri okuma
     todoRef.on("value", snapshot => {
-      // bütün verileri konsola yaz
-      console.log(snapshot.val());
-
-      const todos = snapshot.val();
       const todoList = [];
 
-      // verileri objelerden oluşan bir array haline getir
-      for (let id in todos) {
-        // array'e id'yi de ekleyebilmek için spread operator (...) ile todos[id]'yi tek tek elemanlarına açıyoruz ve id ile birlikte bir JS objesi halinde todoList arrayine push ediyoruz
-        todoList.push({ id, ...todos[id] });
-      }
-      console.log(todoList);
+      // verileri objelerden oluşan bir array haline getiriyoruz. orderBy metodlarına güvenilemeyeceği için, istediğimiz sırayı elde edebilmek için forEach kullanıyoruz https://firebase.google.com/docs/reference/js/firebase.database.DataSnapshot#forEach
+      snapshot.forEach(childSnapshot => {
+        const todos = childSnapshot.val();
+
+        // id'ye buradan erişebildim. bunla ilgili kaynak bulamadım, yerini console'dan kendim buldum
+        let id = childSnapshot.ref_.path.pieces_[1];
+
+        // array'e id'yi de ekleyebilmek için spread operator (...) ile todos'u tek tek elemanlarına açıyoruz ve id ile birlikte bir JS objesi halinde todoList arrayine push ediyoruz
+        todoList.push({ id, ...todos });
+      });
+
       setToDoList(todoList);
     });
   }, []);
